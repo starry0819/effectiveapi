2b7f5f2a2606d0b279b24b40b5114ef02f57757e
zhanghuanfa <997543429@qq.com>
2018-01-12 22:43:05 +0800

    change a little

--- src/main/java/com/zhanghuanfa/model/dto/UserDto.java
+++ src/main/java/com/zhanghuanfa/model/dto/UserDto.java
@@ -1,10 +1,7 @@
 package com.zhanghuanfa.model.dto;
 
 import com.zhanghuanfa.model.po.User;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.io.Serializable;
 
@@ -13,9 +10,9 @@ import java.io.Serializable;
  * @date 2018-01-02 20:14
  */
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 class UserDto extends User implements Serializable {
     private static final long serialVersionUID = 7559704555914671472L;
     /**用户所属机构名称*/
--- src/main/java/com/zhanghuanfa/model/po/User.java
+++ src/main/java/com/zhanghuanfa/model/po/User.java
@@ -14,11 +14,12 @@ import java.io.Serializable;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 public class User implements Serializable {
     private static final long serialVersionUID = 6656048183360808147L;
     /**主键id*/
     private Integer id;
     /**用户名*/
     private String name;
+    /**用户密码*/
+    private String password;
 }
